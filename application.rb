77dab95e1f23428684937f027b9f1fc993447f18
Andrew Nero <andrew.nero@gmail.com>
2014-10-02 23:15:27 -0400

    i hope so

--- application.rb
+++ application.rb
@@ -1,49 +1,67 @@
-before do
-  content_type :json
-  headers 'Access-Control-Allow-Origin' => '*',
-          'Access-Control-Allow-Methods' => ['POST']
-end
+set :public, Proc.new { File.join(root, "_site") }
 
-set :protection, false
-set :public_dir, Proc.new { File.join(root, "_site") }
 
-post '/send_email' do
+post '/send' do  
   if recaptcha_valid?
-    res = Pony.mail(
-      :from => params[:name] + "<" + params[:email] + ">",
-      :to => 'andrew.nero@gmail.com',
-      :subject => "pllease " + params[:subject],
-      :body => params[:message],
-      :via => :smtp,
-      :via_options => {
-        :address              => 'smtp.sendgrid.net',
-        :port                 => '587',
-        :enable_starttls_auto => true,
-        :user_name            => ENV['SENDGRID_USERNAME'],
-        :password             => ENV['SENDGRID_PASSWORD'],
-        :authentication       => :plain,
-        :domain               => 'heroku.com'
-      })
-    content_type :json
-    if res
-      { :message => 'success' }.to_json
-    else
-      { :message => 'failure_email' }.to_json
-    end
+    session[:captcha] = true
+    { :message => 'success' }.to_json
   else
-    { :message => 'failure_captcha' }.to_json
+    session[:captcha] = false
+    { :message => 'success' }.to_json
   end
 end
 
-not_found do
-  File.read('_site/404.html')
+post '/send_email' do  
+	
+
+	# -*- encoding: utf-8 -*-
+	require 'sendgrid_ruby'
+	require 'sendgrid_ruby/version'
+	require 'sendgrid_ruby/email'
+	require 'dotenv'
+
+	config = Dotenv.load
+	sendgrid_username = ENV["SENDGRID_USERNAME"]
+	sendgrid_password = ENV["SENDGRID_PASSWORD"]
+	from = ENV["FROM"]
+	tos = ENV["TOS"].split(',')
+
+	email = SendgridRuby::Email.new
+	email.set_tos(tos)
+	.set_from(from)
+	.set_from_name("From name")
+	.set_subject("[sendgrid-ruby-example] Owl named fullname")
+	.set_text("familyname, what are you doing?\r\n He is in place.")
+	.set_html("<strong> familyname, what are you doing?</strong><br />He is in place.")
+	.add_section('office', 'Nakano')
+	.add_section('home', 'Meguro')
+	.add_category('Category1')
+	.add_header('X-Sent-Using', 'SendGrid-API')
+
+	sendgrid = SendgridRuby::Sendgrid.new(sendgrid_username, sendgrid_password)
+	#sendgrid.debug_output = true # remove comment if you need to see the request
+	response = sendgrid.send(email)
+	puts response
+
+	:message => 'success' }.to_json
+
+
 end
 
-get '/*' do
-  file_name = "_site#{request.path_info}/index.html".gsub(%r{\/+},'/')
-  if File.exists?(file_name)
+
+before do  
+    response.headers['Cache-Control'] = 'public, max-age=36000'
+end
+
+not_found do  
+    File.read('_site/404.html')
+end
+
+get '/*' do  
+    file_name = "_site#{request.path_info}/index.html".gsub(%r{\/+},'/')
+    if File.exists?(file_name)
     File.read(file_name)
-  else
+    else
     raise Sinatra::NotFound
-  end
+    end
 end
--- config.ru
+++ config.ru
@@ -1,11 +1,10 @@
-require 'rubygems'
-require 'sinatra'
-require 'json'
+require 'rubygems'  
+require 'sinatra'  
 require 'rack/recaptcha'
-require 'pony'
 
 use Rack::Recaptcha, :public_key => '6LdIIvsSAAAAALI3bWd2cp-MZAMxYc9ZwmPNexpZ', :private_key => '6LdIIvsSAAAAAADWt6Ti8laUDcJvNXKOFy_oVLEw'
-helpers Rack::Recaptcha::Helpers
+helpers Rack::Recaptcha::Helpers  
+enable :sessions
 
-require './application'
+require './application'  
 run Sinatra::Application
